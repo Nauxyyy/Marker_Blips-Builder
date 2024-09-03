67d3441136eacc0d7eea905c4549629d5654346c
Nauxyy <51507695+Nauxyyy@users.noreply.github.com>
2024-09-03 17:32:23 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Blips & Marker builder'
+author 'Nauxyy'
+
+client_script {
+	'blips.lua',
+	'marker.lua'
+}
---
+++ marker.lua
@@ -0,0 +1,9 @@
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+
+DrawMarker(5, 1664.8912353516, 2605.1101074219, 45.56470489502, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 60, 255, 207, 155, true, true)
+
+	end
+end)
